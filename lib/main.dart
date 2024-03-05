aa82818234de214d767047a6609e9a038d8bc4a2
RELIC7663 <alexispepinosa28@gmail.com>
2024-03-05 00:55:50 -0500

    Login
    New upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,152 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
       home: Scaffold(
+        body: VistaIdentificacionWidget(),
+      ),
+    );
+  }
+}
+
+class VistaIdentificacionWidget extends StatefulWidget {
+  const VistaIdentificacionWidget({Key? key}) : super(key: key);
+
+  @override
+  _VistaIdentificacionWidgetState createState() =>
+      _VistaIdentificacionWidgetState();
+}
+
+class _VistaIdentificacionWidgetState
+    extends State<VistaIdentificacionWidget> {
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (_focusNode.canRequestFocus) {
+          FocusScope.of(context).requestFocus(_focusNode);
+        } else {
+          FocusScope.of(context).unfocus();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xFF192B71),
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          backgroundColor: const Color(0xFF112659),
+          automaticallyImplyLeading: false,
+          title: Align(
+            alignment: AlignmentDirectional(0, 0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(0),
+              child: Image.asset(
+                'assets/images/Title.png',
+                width: 217,
+                height: 47,
+                fit: BoxFit.contain,
+                alignment: Alignment(0, 0),
+              ),
+            ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 0,
         ),
-        body: const Center(
-          child: Text('como estas'),
+        body: SafeArea(
+          top: true,
+          bottom: true,
+          child: Align(
+            alignment: AlignmentDirectional(-1, -1),
+            child: SingleChildScrollView(
+              child: Container(
+                color: const Color(0xFF192B71),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        'assets/images/asd.png',
+                        width: 400.25,
+                        height: 358,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            child: TextFormField(
+                              focusNode: _focusNode,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                hintText: 'Ingrese su email',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                contentPadding: const EdgeInsets.all(24),
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {
+                              print('Sign In button pressed...');
+                            },
+                            child: const Text('Sign Inasdasd'),
+                            style: ElevatedButton.styleFrom(
+                              
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextButton(
+                            onPressed: () {
+                              print('Forgot Password button pressed...');
+                            },
+                            child: const Text('Forgot Password'),
+                            style: TextButton.styleFrom(
+                              
+                              backgroundColor: Colors.grey[300],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
